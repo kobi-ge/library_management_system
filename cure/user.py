1509386dcff642f91df4e32e5b0f44cb690e2bf1
Moshe <moshe@example.com>
2025-11-10 16:35:57 +0200

    Fix user.py

--- cure/user.py
+++ cure/user.py
@@ -3,16 +3,23 @@
 class User:
 
     def __init__(self, name:str, user_id:int):
-        self. name = name
-        self.user_id = user_id
+        self._name = name
+        self._user_id = user_id
         self.borrowed_books = []
 
     def add_book(self,book):
         self.borrowed_books.append(book)
 
-    def get_id(self):
-        return self.user_id
+    def remove_book(self,book):
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
 
-    def show_count(self):
+    def get_user_name(self):
+        return self._name
+
+    def get_id(self):
+        return self._user_id
+
+    def show_books(self):
         for book in self.borrowed_books:
             print(book.get_book_title())
