ec82956d2e09db447edee2a52f133774938ee9f3
Moshe <moshe@example.com>
2025-11-10 11:03:21 +0200

    finish class book

---
+++ cure/book.py
@@ -0,0 +1,22 @@
+
+
+class Book:
+
+    def __init__(self,title:str,author:str,isbn):
+        self._title = title
+        self._author = author
+        self._isbn = isbn
+        self.is_available = True
+
+    def get_isbn_book(self):
+        return self._isbn
+
+    def borrow_book(self):
+        if self.is_available:
+            self.is_available = False
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return f'title:{self._title}\nauthor:{self._author}\nISBN:{self._isbn}'
