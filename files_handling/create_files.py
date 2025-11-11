e328b7fcfff9b23483c03a904e9bf4443379d891
kobi <kodigershuni@gmail.com>
2025-11-11 10:28:02 +0200

    updating files creation to automatically create both files

--- files_handling/create_files.py
+++ files_handling/create_files.py
@@ -1,17 +1,22 @@
 
-class CreateFile:
-    def __init__(self, library_name, file_type):
+class CreateFiles:
+    def __init__(self, library_name):
         self.library_name = library_name
-        self.file_type = file_type
         self.name = ""
+        self.file_type = ["books", "users"]
 
-    def set_name(self):
-        self.name = self.library_name + self.file_type + ".json"
+    def set_name(self, num):
+        self.name = self.library_name + self.file_type[num] + ".json"
         return self.name
 
-    def create_users_file(self):
+    def create_file(self):
         try:
             with open(self.name, "x") as file:
                 pass
         except FileExistsError:
             print(f"file {self.name} already exists")
+
+    def run_creation(self):
+        for file_name in range(2):
+            self.set_name(file_name)
+            self.create_file()
